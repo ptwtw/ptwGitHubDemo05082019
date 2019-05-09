723469f067f47792bc291a4221625f79e21e5a28
ptwtw <tnvrchau@gmail.com>
2019-05-08 23:32:13 -0400

    3rd Commit

--- ptwGitHub/src/main/java/ptwGitHub/MathCalculation.java
+++ ptwGitHub/src/main/java/ptwGitHub/MathCalculation.java
@@ -1,12 +1,30 @@
 package ptwGitHub;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class MathCalculation 
 {
+	 @BeforeTest
+	 public void Header()
+	 {
+		 System.out.println("Welcom to PTW");
+		 System.out.println("------------------------");
+	 }
+	
+	
 	@Test
 	public void Addition()
 	{
 		System.out.println("Addition is " + 0);
 	}
+	
+	@AfterTest
+	 public void Footer()
+	 {
+		 System.out.println("Thank for using our app");
+		 System.out.println("------------------------");
+		
+	 }
 }
